14b17d2ece05df8e35afe9a60250a9d29f02c263
leemet16 <leeackerman@gmail.com>
2022-06-18 16:14:36 -0600

    added name of contract

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -1,3 +1,14 @@
 // SPDX-License-Identifier: MIT
+//taking inspiration from DAPPUniversity tutorial on NFT marketplaces
+//published via MIT license as well. https://github.com/dappuniversity/nft_marketplace
+
+
 pragma solidity ^0.8.1;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "hardhat/console.sol";
+
+contract Marketplace is ReentrancyGuard
+{
+    
+}
