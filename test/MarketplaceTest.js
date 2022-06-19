878c87818f655a5c1b2950bbe9699c8d16fea9d0
leemet16 <leeackerman@gmail.com>
2022-06-19 13:40:50 -0600

    working on test case (and code)

--- test/MarketplaceTest.js
+++ test/MarketplaceTest.js
@@ -7,27 +7,42 @@ const { ethers } = require('hardhat');
 //TREE = 3 and 10**10;
 //HIO = 4 and 10**10;
 
+beforeEach(async function () {
+  let charityRoyalty = 100;
+  accounts = await ethers.getSigners();
 
-describe('Marketplace', function () {
-    it("Testing the marketplace interaction with tokens", async function () {
-      
-      //const accounts = await ethers.getSigners();
-      
-      //set up our rewild contract
-      //const Rewild = await ethers.getContractFactory('Rewild');
-      //const reWild = await Rewild.deploy();
-      //await reWild.deployed();
-      //reWildAddress = reWild.address;
+  //first deploy Rewild contract
+  Rewild = await ethers.getContractFactory('Rewild');
+  reWild = await Rewild.deploy();
+  await reWild.deployed();
 
-      //and now set up the marketplace contract
-      //const Marketplace = await ethers.getContractFactory('Marketplace');
-      //const marketplace = await Marketplace.deploy();
-      //await marketplace.deployed();
-      //marketplaceAddress = marketplace.address;
+  //and now, deploy the marketplace contract
+  Marketplace = await ethers.getContractFactory('Marketplace');
 
-      //do
+  //to deploy, remember that the constructor takes the royalty percentage!
+  marketplace = await Marketplace.deploy(charityRoyalty);
+  await marketplace.deployed();
+  
+  reWildAddress = reWild.address;
+  marketplaceAddress = marketplace.address;
 
-      
+});
 
-    });
+describe("Deployment", function () {
+
+  it("Should track feeAccount and feePercent of the marketplace", async function () {
+    expect(await marketplace.charityAccount()).to.equal(reWild.address);
+    expect(await marketplace.charityRoyalty()).to.equal(charityRoyalty);
   });
+});
+
+describe('Seeds minted', function () {
+  it("mint seeds and check balances", async function () {
+
+    //check balances for each of our tokens - seeds
+    const seedBalance = await reWild.balanceOf(accounts[0].address, 0)
+    expect(10**10).to.equal(Number(seedBalance.toString()));
+
+  });
+});
+
